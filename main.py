3c7a9a69738c91f647cdb0c92ca13a65e1e4a371
aphananthe42 <japaneseindian0246@gmail.com>
2021-10-24 21:57:55 +0900

    Fix: space between import

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
-import tweepy
+
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+import tweepy
 
 def login():
         driver.get(LOGIN_URL)
