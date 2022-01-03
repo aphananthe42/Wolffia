383ba685f55b4085649b2b4ab55ecb3d626fd4f7
Aphananthe <aphananthe42@gmail.com>
2022-01-03 23:46:07 +0900

    Fix: error handling

--- lambda_function.py
+++ lambda_function.py
@@ -5,14 +5,17 @@ import twitter
 
 
 def lambda_handler(event, context):
-    try:
-        twitter.login()
-        twitter.tweet()
-        twitter.logout()
-    except NoSuchElementException:
-        twitter.tweet()
-        twitter.logout()
-    except Exception as e:
-        line.send_line_notify(e)
-    finally:
-        twitter.driver_quit()
+    for _ in range(3):
+        try:
+            twitter.login()
+            twitter.tweet()
+            twitter.logout()
+        except (IndexError, NoSuchElementException):
+            twitter.logout()
+        except Exception as e:
+            twitter.logout()
+            line.send_line_notify(e)
+        else:
+            break
+        finally:
+            twitter.driver_quit()
