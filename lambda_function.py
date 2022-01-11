3708e6eb74ee59ad697ea1864884e6ff8d4747c4
Aphananthe <aphananthe42@gmail.com>
2022-01-11 18:21:58 +0900

    Fix: error handling

--- lambda_function.py
+++ lambda_function.py
@@ -3,15 +3,13 @@ import twitter
 
 
 def lambda_handler(event, context):
+    twitter.login()
     for _ in range(3):
         try:
-            twitter.login()
             twitter.tweet()
-            twitter.logout()
         except Exception as e:
-            twitter.logout()
             line.send_line_notify(e)
         else:
-            break
-        finally:
+            twitter.logout()
             twitter.driver_quit()
+            break
